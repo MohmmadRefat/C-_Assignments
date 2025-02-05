5b8a4bcccca4bb1402aeed01ae0ad13e94492f27
Mohmad Refat <mohmadrefat2004@gmail.com>
2025-02-06 01:43:09 +0200

    1- Given a Queue, implement a function to reverse the elements of a queue using a stack.

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -2,9 +2,37 @@
 {
     internal class Program
     {
+        public static void ReverseElementsUsingStack<T>(Queue<T> queue)
+        {
+            Stack<T> stack = new Stack<T>();
+            while (queue.Count>0)
+            {
+                stack.Push(queue.Dequeue());
+            }
+
+            while (stack.Count > 0)
+            {
+                queue.Enqueue(stack.Pop());
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 1 Given a Queue, implement a function to reverse the elements of a queue using a stack.
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+
+            foreach (var item in queue)
+                Console.WriteLine(item);
+            Console.WriteLine("----------- AFTER Reverse --------------");
+            ReverseElementsUsingStack<int>(queue);
+            foreach (var item in queue)
+                Console.WriteLine(item); 
+            #endregion
+
         }
     }
 }
