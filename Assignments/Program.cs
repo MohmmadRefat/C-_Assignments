76a397bb8925c9da80c563f6c2a96c1774c2c707
Mohmad Refat <mohmadrefat2004@gmail.com>
2025-02-09 06:13:38 +0200

    1-Generics Collections (LinkedList)

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region LinkedList
+            LinkedList<int>linkedList = new LinkedList<int>();
+            LinkedListNode<int>FirstNode= linkedList.AddLast(1);
+            LinkedListNode<int>SecondNode= linkedList.AddLast(2);
+            LinkedListNode<int>ThirdNode= linkedList.AddLast(3);
+            LinkedListNode<int>FourthNode= linkedList.AddLast(4);
+            LinkedListNode<int> FifthNode = linkedList.AddFirst(5);
+            Console.WriteLine($"FirstNode: {FirstNode.Value}");
+            Console.WriteLine($"FirstNode.Pervious.Value : {FirstNode.Previous?.Value??0}");
+            Console.WriteLine($"FirstNode.Next.Value : {FirstNode.Next?.Value ?? 0}");
+            Console.WriteLine("-- ----------------------");
+
+            Console.WriteLine($"SecondeNode: {SecondNode.Value}");
+            Console.WriteLine($"ThirdNode: {ThirdNode.Value}");
+            Console.WriteLine("-- ----------------------");
+            Console.WriteLine($"FourthNode: {FourthNode.Value}");
+            Console.WriteLine($"FourthNode.Pervious.Value : {FourthNode.Previous?.Value ?? 0}");
+            Console.WriteLine($"FourthNode.Next.Value : {FourthNode.Next?.Value ?? 0}");
+
+            Console.WriteLine($"FifthNode : {FifthNode.Value}");
+            #endregion
+
+
         }
     }
 }
