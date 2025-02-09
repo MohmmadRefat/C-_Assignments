741e2ddda47b9ed625278d3befdbc8699d6c5773
Mohmad Refat <mohmadrefat2004@gmail.com>
2025-02-09 06:28:16 +0200

    2- Generics Collections (Stack & Queue)

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -5,26 +5,67 @@
         static void Main(string[] args)
         {
             #region LinkedList
-            LinkedList<int>linkedList = new LinkedList<int>();
-            LinkedListNode<int>FirstNode= linkedList.AddLast(1);
-            LinkedListNode<int>SecondNode= linkedList.AddLast(2);
-            LinkedListNode<int>ThirdNode= linkedList.AddLast(3);
-            LinkedListNode<int>FourthNode= linkedList.AddLast(4);
-            LinkedListNode<int> FifthNode = linkedList.AddFirst(5);
-            Console.WriteLine($"FirstNode: {FirstNode.Value}");
-            Console.WriteLine($"FirstNode.Pervious.Value : {FirstNode.Previous?.Value??0}");
-            Console.WriteLine($"FirstNode.Next.Value : {FirstNode.Next?.Value ?? 0}");
-            Console.WriteLine("-- ----------------------");
+            //LinkedList<int>linkedList = new LinkedList<int>();
+            //LinkedListNode<int>FirstNode= linkedList.AddLast(1);
+            //LinkedListNode<int>SecondNode= linkedList.AddLast(2);
+            //LinkedListNode<int>ThirdNode= linkedList.AddLast(3);
+            //LinkedListNode<int>FourthNode= linkedList.AddLast(4);
+            //LinkedListNode<int> FifthNode = linkedList.AddFirst(5);
+            //Console.WriteLine($"FirstNode: {FirstNode.Value}");
+            //Console.WriteLine($"FirstNode.Pervious.Value : {FirstNode.Previous?.Value??0}");
+            //Console.WriteLine($"FirstNode.Next.Value : {FirstNode.Next?.Value ?? 0}");
+            //Console.WriteLine("-- ----------------------");
 
-            Console.WriteLine($"SecondeNode: {SecondNode.Value}");
-            Console.WriteLine($"ThirdNode: {ThirdNode.Value}");
-            Console.WriteLine("-- ----------------------");
-            Console.WriteLine($"FourthNode: {FourthNode.Value}");
-            Console.WriteLine($"FourthNode.Pervious.Value : {FourthNode.Previous?.Value ?? 0}");
-            Console.WriteLine($"FourthNode.Next.Value : {FourthNode.Next?.Value ?? 0}");
+            //Console.WriteLine($"SecondeNode: {SecondNode.Value}");
+            //Console.WriteLine($"ThirdNode: {ThirdNode.Value}");
+            //Console.WriteLine("-- ----------------------");
+            //Console.WriteLine($"FourthNode: {FourthNode.Value}");
+            //Console.WriteLine($"FourthNode.Pervious.Value : {FourthNode.Previous?.Value ?? 0}");
+            //Console.WriteLine($"FourthNode.Next.Value : {FourthNode.Next?.Value ?? 0}");
 
-            Console.WriteLine($"FifthNode : {FifthNode.Value}");
+            //Console.WriteLine($"FifthNode : {FifthNode.Value}");
             #endregion
+
+            #region Stack & Queue
+            //Stack<int>stack=new Stack<int>();
+            //stack.Push(1);
+            //stack.Push(2); 
+            //stack.Push(3);
+
+
+            //Console.WriteLine($"Stack Peek : {stack.TryPeek(out int result)}");
+            //Console.WriteLine(result);
+
+
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    Console.WriteLine($"Stack Pop : {stack.TryPop(out int result)}");
+            //    Console.WriteLine(result);
+            //}
+
+            //foreach(var item in stack)
+            //    Console.WriteLine(item);
+
+            //--------------------------------------------------------//
+
+            //Queue<int>numbers = new Queue<int>();
+            //numbers.Enqueue(1);
+            //numbers.Enqueue(2);
+            //numbers.Enqueue(3);
+
+            //foreach (var item in numbers)
+            //    Console.WriteLine(item);
+            //Console.WriteLine("---------------");
+            //numbers.Dequeue();
+            //numbers.Dequeue();
+            //numbers.Dequeue();
+            //numbers.TryDequeue(out int result);
+            //Console.WriteLine("----------------"); 
+            #endregion
+
+
+
+
 
 
         }
