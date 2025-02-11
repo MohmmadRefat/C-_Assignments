f7d0f16cbaa8b08608b3ee331f435da8fe5c5096
Mohmad Refat <mohmadrefat2004@gmail.com>
2025-02-11 06:11:11 +0200

    The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases, which makes it inefficient for large datasets. How we can optimize the Bubble Sort algorithm And implement the code of this optimized bubble sort algorithm

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -2,9 +2,47 @@
 {
     internal class Program
     {
+        public static void Swap<T>(ref T X,ref T Y)
+        {
+            T temp;
+            temp = X;
+            X = Y;
+            Y = temp;
+        }
+        public static void BubbleSort<T>(T[] arr,Func<T,T,bool>Compare)
+        {
+            bool swapped = false;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for(int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (Compare(arr[j], arr[j+1]))
+                    {
+                        Swap<T>(ref arr[j], ref arr[j + 1]);
+                        swapped = true;
+                    }
+                }
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 1 -The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases, which makes it inefficient for large datasets. How we can optimize the Bubble Sort algorithm And implement the code of this optimized bubble sort algorithm
+
+            //we can decrease the time of the Bubble sort algorithm to O(N) By use Boolean Flag to detect if the swap operation is occured or not 
+            //if the swap operation occured that's means the Array/List is still not sorted , other wise means it is been sorted
+            // this optimazation in Best case only O(N)
+
+            int[] numbers = { 10, 2, 3, 5, 6, 7, 1, 8, 9, 4 };
+            BubbleSort<int>(numbers, delegate (int X, int Y) { return X > Y; });
+            foreach (int x in numbers)
+                Console.WriteLine( x);
+
+
+            #endregion
         }
     }
 }
